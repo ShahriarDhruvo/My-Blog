e2b297ee8b2e0079ba3bcb9be2a933b7bfae68e8
ShahriarDhruvo <shahriarelahi3062@gmail.com>
2020-04-02 21:46:52 +0600

    Files update (2)

---
+++ Environments/Project001_env_18March2020/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/thephilosopher/Documents/Django-projects/Environments/Project001_env_18March2020/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
