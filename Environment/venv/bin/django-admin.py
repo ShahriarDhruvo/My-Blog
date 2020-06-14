1287bf6af59e5e1c23ea7a6fb191625a1004959d
ShahriarDhruvo <shahriarelahi3062@gmail.com>
2020-06-14 20:31:08 +0600

    Second release

--- Environment/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/thephilosopher/Documents/Web Development Practice/Django-projects/My-Blog/Environment/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
